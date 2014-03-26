38d129fbfb2a64b1075a629a272d6a7febab01d5
wyixin <single.w.1992@gmail.com>
2014-03-26 21:49:30 +0800

    modify Compile.php

--- programing/php/phpTemplate/Compile.php
+++ programing/php/phpTemplate/Compile.php
@@ -4,8 +4,16 @@ class Compile{
 
     function run($source, $dest){
         $content = file_get_contents($source);
-        $content = preg_replace_callback("#\{([a-zA-Z_][a-zA-Z0-9_]*)\}#im", function($matches){return "<?php echo \$$matches[1];?>";}, $content);
+        $content = preg_replace_callback("#\{([a-zA-Z_][a-zA-Z0-9_]*)\}#im", array($this, "_VAR"), $content);
+        
         file_put_contents($dest, $content);
     }
 
+    private function _VAR($matches){
+        return '<?php echo $' . $matches[1] . ';?>';    
+    }
+
+    private function _IF(){}
+    private function _FOREACH(){}
+    private function _EVAL(){}
 }
