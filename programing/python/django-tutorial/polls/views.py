1431ca22396441341362a8a51dd83464e665bdbb
wyixin <single.w.1992@gmail.com>
2014-03-27 16:37:48 +0800

    tutorial part 2

--- programing/python/django-tutorial/polls/views.py
+++ programing/python/django-tutorial/polls/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world form index page")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    #poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})    
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
